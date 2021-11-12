7f81c37b81a3539fac24733de8f922f8fc475b87
Joey <30815220+joeylking@users.noreply.github.com>
2021-11-12 15:43:10 -0600

    Previous and Next Book Buttons

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,17 +15,47 @@ class App extends Component {
     };
   }
 
+  nextBook = () => {
+    let tempBookNumber = this.state.bookNumber;
+    tempBookNumber++;
+    if (tempBookNumber === this.books.length) {
+      tempBookNumber = 0;
+    }
+    this.setState({
+      bookNumber: tempBookNumber,
+    });
+  };
+
+  previousBook = () => {
+    let tempBookNumber = this.state.bookNumber;
+    tempBookNumber--;
+    if (tempBookNumber < 0) {
+      tempBookNumber = 2;
+    }
+    this.setState({
+      bookNumber: tempBookNumber,
+    });
+  };
+
   render() {
     return (
       <div className='container-fluid'>
         <TitleBar />
-        <div className='col-md-4'>{/* Previous Book Button */}</div>
-        <div className='col-md-4'>
-          {/* Book Cover */}
-          <h1>{this.books[this.state.bookNumber].title}</h1>
-          <h4>{this.books[this.state.bookNumber].author}</h4>
+        <div className='row'>
+          <div className='col-md-4'>
+            {/* Previous Book Button */}
+            <button onClick={this.previousBook}>Previous</button>
+          </div>
+          <div className='col-md-4'>
+            {/* Book Cover */}
+            <h1>{this.books[this.state.bookNumber].title}</h1>
+            <h4>{this.books[this.state.bookNumber].author}</h4>
+          </div>
+          <div className='col-md-4'>
+            {/* Next Book Button */}
+            <button onClick={this.nextBook}>Next</button>
+          </div>
         </div>
-        <div className='col-md-4'>{/* Next Book Button */}</div>
       </div>
     );
   }
